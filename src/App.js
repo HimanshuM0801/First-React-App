a83df91a3029d69ab54ce4ed08e56d49db1b7d69
Indresh <indresh.lucky101@gmail.com>
2023-01-04 11:24:16 +0530

    app.js file changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [value,setvalue] = useState("");
   return (
     <div className="App">
       <h1>
         This is created by Indresh
       </h1>
+      
       <div>
         lffhderhrnhjrgerhgerkgrngerrgegernbgregnegrnegrnernhreherhenernhrehrehrnehrhrenhrenhrnrhehernhehrnnhrh
         rhrehehrehrehrehehrehrehreh frebrbreer r ehrththrh creaetd by indresh
       </div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis reprehenderit itaque aliquam corporis earum fugiat rem, corrupti deleniti ut quis quas tenetur ullam sed voluptates commodi doloremque sit, doloribus officia provident asperiores architecto quod obcaecati aliquid nemo. Ipsa molestiae sunt et voluptatum accusamus magnam unde, minima consectetur natus itaque illum.</div>
+      <h1>Indresh Propose Changes</h1>
+      <h1>Indresh Propose Changes</h1>
+      <h1>Indresh Propose Changes</h1>
+      <h1>Indresh Propose Changes</h1>
+      <h1>Indresh Propose Changes</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
